1c5b7edf50612ac1b984bbefe0244ac543fb6769
Hrutvik Yadav <97113293+hrutvikyadav@users.noreply.github.com>
2026-01-09 23:30:48 +0530

    refactor: constants

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -2,7 +2,8 @@ package iteration
 
 // Repeat takes in a string and repeats it 5 times
 func Repeat(char string) (repeated string ) {
-	for i := 0; i < 5; i++ {
+	repeatCount := 5
+	for i := 0; i < repeatCount; i++ {
 		repeated += char
 	}
 	return
