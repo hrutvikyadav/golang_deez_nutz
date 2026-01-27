9bddefb38fbcb0d14359760b6ab50e02611c7842
hrutvikyadav <hrutvikyadav@gmail.com>
2026-01-27 14:07:16 +0530

    refactor: use interface
    - decouples the consumer of the shapes from the details of which shape
      is being refered to

--- structs_methods_interfaces/shapes_test.go
+++ structs_methods_interfaces/shapes_test.go
@@ -12,25 +12,27 @@ func TestPerimeter(t *testing.T) {
 	}
 }
 
-func TestArea (t *testing.T) {
-	t.Run("for rectangles", func(t *testing.T) {
-		rectangle := Rectangle{ 10.0, 12.0 }
-		got := rectangle.Area()
-		want := 120.0
 
+func TestArea (t *testing.T) {
+	checkArea := func (t testing.TB, shape Shape, want float64) {
+		got := shape.Area()
 		if got != want {
 			t.Errorf("got %.2f want %.2f", got, want)
 		}
+	}
+
+	t.Run("for rectangles", func(t *testing.T) {
+		rectangle := Rectangle{ 10.0, 12.0 }
+		want := 120.0
+
+		checkArea(t, rectangle, want)
 	})
 
 	t.Run("for circles", func(t *testing.T) {
 		circle := Circle{ 10.0 }
-		got := circle.Area()
 		want := 314.1592653589793
 
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
+		checkArea(t, circle, want)
 	})
 }
 
