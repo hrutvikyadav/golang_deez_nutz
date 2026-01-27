afc7191449a8548b066b9d7c0acfcda0f90e72ab
hrutvikyadav <hrutvikyadav@gmail.com>
2026-01-27 17:45:50 +0530

    feat: err handling for maps, more functionality

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,5 +1,20 @@
 package main
 
-func Search(dict map[string]string, key string) string {
-	return dict[key]
+import "errors"
+
+type Dictionary map[string]string
+
+var ErrWordNotFound = errors.New("word not known")
+
+func (d Dictionary) Search(key string) (string, error) {
+	definition, ok := d[key]
+	if !ok {
+		return "", ErrWordNotFound
+	}
+
+	return definition, nil
+}
+
+func (d Dictionary) Add(key string, definition string) {
+	d[key] = definition
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -3,10 +3,34 @@ package main
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dict := map[string]string{"test": "lulz"}
+	dict := Dictionary{"test": "lulz"}
 
-	definition := Search(dict, "test")
-	want := "lulz"
+	t.Run("search known word", func(t *testing.T) {
+		definition, _ := dict.Search("test")
+		want := "lulz"
+		assertStrings(t, definition, want)
+	})
+
+	t.Run("search unknown word", func(t *testing.T) {
+		_, err := dict.Search("testes")
+		if err == nil {
+			t.Fatal("expected error")
+		}
+		assertError(t, err, ErrWordNotFound)
+	})
+}
+
+func TestAdd(t *testing.T) {
+	dict := Dictionary{"test": "lulz"}
+	dict.Add("testes", "ballz")
+
+	definition, err := dict.Search("testes")
+	want := "ballz"
+
+	if err != nil {
+		t.Fatal("did not expect error:", err)
+	}
+
 	assertStrings(t, definition, want)
 }
 
@@ -14,5 +38,11 @@ func assertStrings(t testing.TB, got, want string) {
 	t.Helper()
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+func assertError(t testing.TB, got, want error) {
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
 	}
 }
