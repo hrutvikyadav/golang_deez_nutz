e8bb294d12d898ef738982c2a1f3a671434f6c91
hrutvikyadav <hrutvikyadav@gmail.com>
2026-01-27 11:33:54 +0530

    chore: rm redundant tests
    - verify with `go test -cover` before and after removing tests

--- arrays_slices/sum_test.go
+++ arrays_slices/sum_test.go
@@ -3,17 +3,6 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-	t.Run("Array of 5 integers", func(t *testing.T) {
-		numbers := []int{1,2,3,4,5} // Changed to be a slice
-
-		got := Sum(numbers)
-		want := 15
-
-		if got != want {
-			t.Errorf("Got: %d, want: %d; given: %v", got, want, numbers)
-		}
-	})
-
 	t.Run("Slice of any number of integers", func(t *testing.T) {
 		numbers := []int{1,2,3,4,5,5} // Slices can have any capacity
 
