facb44e6ab384cae4a4200ec60c4a7235627bcac
hrutvikyadav <hrutvikyadav@gmail.com>
2026-01-27 11:31:19 +0530

    feat: introduce slices(variable capacity)

--- arrays_slices/sum_test.go
+++ arrays_slices/sum_test.go
@@ -3,13 +3,25 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1,2,3,4,5} // Arrays have fixed capacity
-	// numbers := [...]int{1,2,3,4,5} NOTE: capacity can also be infered
+	t.Run("Array of 5 integers", func(t *testing.T) {
+		numbers := []int{1,2,3,4,5} // Changed to be a slice
 
-	got := Sum(numbers)
-	want := 15
+		got := Sum(numbers)
+		want := 15
 
-	if got != want {
-		t.Errorf("Got: %d, want: %d; given: %v", got, want, numbers)
-	}
+		if got != want {
+			t.Errorf("Got: %d, want: %d; given: %v", got, want, numbers)
+		}
+	})
+
+	t.Run("Slice of any number of integers", func(t *testing.T) {
+		numbers := []int{1,2,3,4,5,5} // Slices can have any capacity
+
+		got := Sum(numbers)
+		want := 20
+
+		if got != want {
+			t.Errorf("Got: %d, want: %d; given: %v", got, want, numbers)
+		}
+	})
 }
