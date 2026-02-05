9b19bdf2fc5272f068db316845f5c497eaedc2f0
hrutvikyadav <hrutvikyadav@gmail.com>
2026-02-05 16:56:34 +0530

    feat: mock rev3 with spies and DI

--- mocking/mocking.go
+++ mocking/mocking.go
@@ -4,18 +4,31 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
 const countDownN = 3
 const finalWord = "Go!"
 
-func Countdown(w io.Writer) {
+type Sleeper interface {
+	Sleep()
+}
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func Countdown(w io.Writer, sleeper Sleeper) {
 	for i:= countDownN; i > 0; i-- {
 		fmt.Fprintln(w, i)
+		// time.Sleep(1 * time.Second) // WARN: we now have a dependency on `Sleep`ingwhich we need to extrace out of this function so that we have more control
+		sleeper.Sleep()
 	}
 	fmt.Fprint(w, finalWord)
 }
 
 func main() {
-	Countdown(os.Stdout)
+	defaultSleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, defaultSleeper)
 }
--- mocking/mocking_test.go
+++ mocking/mocking_test.go
@@ -5,10 +5,23 @@ import (
 	"testing"
 )
 
+// If we can mock time.Sleep we can use DI to use the mock instead of a "real" time.Sleep;
+// then we can spy on the calls to make assertions on them.
+// type Sleeper interface {
+// 	Sleep()
+// }
+type SpySleeper struct {
+	Calls int
+}
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
 func TestCountdown(t *testing.T) {
 	buffer := &bytes.Buffer{}
+	spySleeper := &SpySleeper{Calls: 0}
 
-	Countdown(buffer)
+	Countdown(buffer, spySleeper)
 
 	got := buffer.String()
 	want := `3
@@ -18,5 +31,9 @@ Go!`
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
+	}
+
+	if spySleeper.Calls != 3 {
+		t.Errorf("not enough calls to Sleeper want 3 got %d", spySleeper.Calls)
 	}
 }
