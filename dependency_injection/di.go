f07ff2593f8b7e0778a9f81775299ca4f8c6fe1c
hrutvikyadav <hrutvikyadav@gmail.com>
2026-02-05 15:37:09 +0530

    feat: DI, interfaces- more general purpose code
    - swap out concrete types for interfaces and see the polygamy

--- dependency_injection/di.go
+++ dependency_injection/di.go
@@ -1,14 +1,19 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
+	"io"
+	"os"
 )
 
-func Greet(buf *bytes.Buffer, s string) {
+func Greet(writer io.Writer, s string) {
 	// NOTE: `fmt.Printf("Hello, %s", s)` this is the simpler version but it is not testable
 	// Printf prints to standard output which cannot be tested using a test framework.
 	// So we use dependency injection to inject the dependency of Printf (which is identified by digging ballz deep),
 	// this way we pass in (inject the dependency to have more control over printing in a way we can test)
-	fmt.Fprintf(buf, "Hello, %s", s)
+	fmt.Fprintf(writer, "Hello, %s", s)
+}
+
+func main() {
+	Greet(os.Stdout, "TO OUT")
 }
