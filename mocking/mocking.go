a56155e4a955debd21b266cf2cd6aaef1d1c3882
hrutvikyadav <hrutvikyadav@gmail.com>
2026-02-05 18:12:32 +0530

    feat: mock rev5
    - configure (time) sleeper

--- mocking/mocking.go
+++ mocking/mocking.go
@@ -13,22 +13,28 @@ const finalWord = "Go!"
 type Sleeper interface {
 	Sleep()
 }
-type DefaultSleeper struct{}
 
-func (d *DefaultSleeper) Sleep() {
-	time.Sleep(1 * time.Second)
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
 }
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
 
 func Countdown(w io.Writer, sleeper Sleeper) {
 	for i:= countDownN; i > 0; i-- {
 		fmt.Fprintln(w, i)
-		// time.Sleep(1 * time.Second) // WARN: we now have a dependency on `Sleep`ingwhich we need to extrace out of this function so that we have more control
 		sleeper.Sleep()
 	}
 	fmt.Fprint(w, finalWord)
 }
 
 func main() {
-	defaultSleeper := &DefaultSleeper{}
-	Countdown(os.Stdout, defaultSleeper)
+	configurableSleeper := &ConfigurableSleeper{
+		duration: 2 * time.Second,
+		sleep: time.Sleep,
+	}
+	Countdown(os.Stdout, configurableSleeper)
 }
