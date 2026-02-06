a3d6a4a1e510ee3a83e332b378b63eca843a9aba
hrutvikyadav <hrutvikyadav@gmail.com>
2026-02-06 14:45:51 +0530

    feat: sync counter rev2
    - concurrent operations on count
    - use mutex to avoid race conditions

--- sync/counter.go
+++ sync/counter.go
@@ -1,10 +1,17 @@
-package sync
+package syncz
+
+import "sync"
 
 type Counter struct {
+	mu sync.Mutex
 	count int
 }
 
 func (c *Counter) Inc() {
+	// WARN: without mutual exclusion we have race condition
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	//
 	c.count++
 }
 
--- sync/sync_test.go
+++ sync/sync_test.go
@@ -1,6 +1,9 @@
-package sync
+package syncz
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 func TestCounter(t *testing.T) {
 	t.Run("increementing counter 3 times should track the count as 3", func(t *testing.T){
@@ -9,11 +12,31 @@ func TestCounter(t *testing.T) {
 		counter.Inc()
 		counter.Inc()
 
-		assertCount(t, counter, 3)
+		assertCount(t, &counter, 3)
+	})
+
+	t.Run("it runs safely concurrently", func(t *testing.T) {
+		wantedCount := 1000
+		counter := Counter{}
+
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+
+		for i := 0; i < wantedCount; i++ {
+			go func() {
+				counter.Inc()
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+
+		assertCount(t, &counter, wantedCount)
 	})
 }
 
-func assertCount(t testing.TB, counter Counter, want int) {
+// dont pass mutex (Counter) by value, pass by reference instead
+// run `go vet` for more
+func assertCount(t testing.TB, counter *Counter, want int) {
 	got := counter.CountValue()
 
 	if got != want {
