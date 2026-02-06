1b1ed5cf94f1b513edf262858b50c532b4a133eb
hrutvikyadav <hrutvikyadav@gmail.com>
2026-02-06 17:46:26 +0530

    feat: spy responsewriter handle error test cases

--- context/context_test.go
+++ context/context_test.go
@@ -2,6 +2,7 @@ package contezt
 
 import (
 	"context"
+	"errors"
 	"log"
 	"net/http"
 	"net/http/httptest"
@@ -41,6 +42,24 @@ func (s *SpyStore) Fetch(ctx context.Context) (string, error) {
 	}
 }
 
+type SpyResponseWriter struct {
+	written bool
+}
+
+func (s *SpyResponseWriter) Header() http.Header {
+	s.written = true
+	return nil
+}
+
+func (s *SpyResponseWriter) Write([]byte) (int, error) {
+	s.written = true
+	return 0, errors.New("not implemented")
+}
+
+func (s *SpyResponseWriter) WriteHeader(statusCode int) {
+	s.written = true
+}
+
 func TestServer(t *testing.T) {
 	t.Run("return data from store", func(t *testing.T) {
 		data := "hello, world"
@@ -57,20 +76,22 @@ func TestServer(t *testing.T) {
 		}
 	})
 
-	// t.Run("tells store to cancel work if request is cancelled", func(t *testing.T) {
-	// 	data := "hello, world"
-	// 	store := &SpyStore{ data, false, t}
-	// 	svr := Server(store)
-	//
-	// 	request := httptest.NewRequest(http.MethodGet, "/", nil)
-	//
-	// 	cancellingCtx, cancel := context.WithCancel(request.Context())
-	// 	time.AfterFunc(10*time.Millisecond, cancel)
-	// 	request = request.WithContext(cancellingCtx)
-	//
-	// 	response := httptest.NewRecorder()
-	//
-	// 	svr.ServeHTTP(response, request)
-	// 	store.assertCancel()
-	// })
+	t.Run("tells store to cancel work if request is cancelled", func(t *testing.T) {
+		data := "hello, world"
+		store := &SpyStore{data, t}
+		svr := Server(store)
+
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+
+		cancellingCtx, cancel := context.WithCancel(request.Context())
+		time.AfterFunc(10*time.Millisecond, cancel)
+		request = request.WithContext(cancellingCtx)
+
+		response := &SpyResponseWriter{}
+
+		svr.ServeHTTP(response, request)
+		if response.written {
+			t.Error("Should not have written response due to cancellation")
+		}
+	})
 }
