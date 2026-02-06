6e9e46d3a1d3e0d6b994d403056890b693350152
hrutvikyadav <hrutvikyadav@gmail.com>
2026-02-06 11:54:25 +0530

    feat: select with goroutines
    - first channel that is read is *selected* as the winner

--- select/racer.go
+++ select/racer.go
@@ -2,22 +2,22 @@ package racer
 
 import (
 	"net/http"
-	"time"
 )
 
 func Racer(playerone, playertwo string) string {
-	pOneTime := raceWithStopWatch(playerone)
-	pTwoTime := raceWithStopWatch(playertwo)
-
-	if pOneTime < pTwoTime {
+	select {
+	case <- ping(playerone):
 		return playerone
-	} else {
+	case <- ping(playertwo):
 		return playertwo
 	}
 }
 
-func raceWithStopWatch(p string) (time.Duration) {
-	startTime := time.Now()
-	http.Get(p)
-	return time.Since(startTime)
+func ping(url string) chan struct{} {
+	c := make(chan struct{})
+	go func(){
+		http.Get(url)
+		close(c)
+	}()
+	return c
 }
