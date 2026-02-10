dbc64394ffcc71727cea8bc7b9ecb57cdde68efc
hrutvikyadav <hrutvikyadav@gmail.com>
2026-02-10 15:45:44 +0530

    feat: blogrenderer rev1

---
+++ blogrenderer/blogrenderer_test.go
@@ -0,0 +1,35 @@
+package blogrenderer_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/hrutvikyadav/golang_deez_nutz/blogrenderer"
+)
+
+func TestBlogRenderer(t *testing.T) {
+	t.Run("should generate html from Posts", func(t *testing.T) {
+		post := blogrenderer.Post{
+			Title: "Hello once more",
+			Description: "desc",
+			Tags: []string{ "sometag", "othertag" },
+			Body: `# Body
+## some imp
+lorem ipsum dolor emmet lorem ipsum dolor emmet lorem ipsum dolor emmet lorem ipsum dolor emmet lorem ipsum dolor emmet`,
+		}
+
+		buf := bytes.Buffer{}
+		err := blogrenderer.Convert(&buf, post)
+		if err != nil {
+			t.Error(err)
+		}
+
+		got := buf.String()
+		want := "<h1>Hello once more</h1>"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+
+	})
+}
