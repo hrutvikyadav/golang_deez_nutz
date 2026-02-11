96232955b9a3cc7f604711200eae04aca2d515fa
hrutvikyadav <hrutvikyadav@gmail.com>
2026-02-11 15:17:47 +0530

    refactor: constructor

--- generics/generics_test.go
+++ generics/generics_test.go
@@ -49,7 +49,7 @@ func AssertFalse(t *testing.T, got bool) {
 
 func TestStack(t *testing.T) {
 	t.Run("integer stack", func(t *testing.T) {
-		myStackOfInts := new(genericz.Stack[int])
+		myStackOfInts := genericz.NewStack[int]()
 
 		// check stack is empty
 		AssertTrue(t, myStackOfInts.IsEmpty())
