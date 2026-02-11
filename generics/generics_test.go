d3ade572bbdf7df11120ad1104d7213e656072df
hrutvikyadav <hrutvikyadav@gmail.com>
2026-02-11 15:10:45 +0530

    feat(generics): generics
    - generate code for free based on T (DRY)
    - type safety, as opposed to interface{}

--- generics/generics_test.go
+++ generics/generics_test.go
@@ -49,7 +49,7 @@ func AssertFalse(t *testing.T, got bool) {
 
 func TestStack(t *testing.T) {
 	t.Run("integer stack", func(t *testing.T) {
-		myStackOfInts := new(genericz.StackOfInts)
+		myStackOfInts := new(genericz.Stack[int])
 
 		// check stack is empty
 		AssertTrue(t, myStackOfInts.IsEmpty())
@@ -65,42 +65,12 @@ func TestStack(t *testing.T) {
 		value, _ = myStackOfInts.Pop()
 		AssertEqual(t, value, 123)
 		AssertTrue(t, myStackOfInts.IsEmpty())
-	})
 
-	t.Run("string stack", func(t *testing.T) {
-		myStackOfStrings := new(genericz.StackOfStrings)
-
-		// check stack is empty
-		AssertTrue(t, myStackOfStrings.IsEmpty())
-
-		// add a thing, then check it's not empty
-		myStackOfStrings.Push("123")
-		AssertFalse(t, myStackOfStrings.IsEmpty())
-
-		// add another thing, pop it back again
-		myStackOfStrings.Push("456")
-		value, _ := myStackOfStrings.Pop()
-		AssertEqual(t, value, "456")
-		value, _ = myStackOfStrings.Pop()
-		AssertEqual(t, value, "123")
-		AssertTrue(t, myStackOfStrings.IsEmpty())
-	})
-
-	t.Run("interface stack DX is horrid", func(t *testing.T) {
-		myStackOfInts := new(genericz.StackOfInts)
-
+		// can get the numbers we put in as numbers, not untyped interface{}
 		myStackOfInts.Push(1)
 		myStackOfInts.Push(2)
 		firstNum, _ := myStackOfInts.Pop()
 		secondNum, _ := myStackOfInts.Pop()
-		// WARN: cant do this because cannot use `+` operator on interface{} `AssertEqual(t, firstNum+secondNum, 3)`
-		// get our ints from out interface{}
-		reallyFirstNum, ok := firstNum.(int)
-		AssertTrue(t, ok) // need to check we definitely got an int out of the interface{}
-
-		reallySecondNum, ok := secondNum.(int)
-		AssertTrue(t, ok) // and again!
-
-		AssertEqual(t, reallyFirstNum+reallySecondNum, 3)
+		AssertEqual(t, firstNum+secondNum, 3)
 	})
 }
