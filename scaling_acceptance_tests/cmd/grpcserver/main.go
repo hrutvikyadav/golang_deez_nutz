5219cd048cc00fa1ba96500c8917f8de3fa52cf5
hrutvikyadav <hrutvikyadav@gmail.com>
2026-02-16 17:59:16 +0530

    refactor(s_a_t): grpc

--- scaling_acceptance_tests/cmd/grpcserver/main.go
+++ scaling_acceptance_tests/cmd/grpcserver/main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"context"
 	"log"
 	"net"
 
 	"github.com/hrutvikyadav/go-specs-greet/adapters/grpcserver"
-	"github.com/hrutvikyadav/go-specs-greet/domain/interactions"
 	"google.golang.org/grpc"
 )
 
@@ -16,17 +14,9 @@ func main() {
 		log.Fatal(err)
 	}
 	s := grpc.NewServer()
-	grpcserver.RegisterGreeterServer(s, &GreetServer{})
+	grpcserver.RegisterGreeterServer(s, &grpcserver.GreetServer{})
 
 	if err := s.Serve(lis); err != nil {
 		log.Fatal(err)
 	}
-}
-
-type GreetServer struct {
-	grpcserver.UnimplementedGreeterServer
-}
-
-func (g GreetServer) Greet(ctx context.Context, request *grpcserver.GreetRequest) (*grpcserver.GreetReply, error) {
-	return &grpcserver.GreetReply{Message: interactions.Greet(request.Name)}, nil
 }
