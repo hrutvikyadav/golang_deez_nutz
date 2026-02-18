0a1752cda5ce47a9c9c728949c786ac69eb24495
hrutvikyadav <hrutvikyadav@gmail.com>
2026-02-18 16:39:02 +0530

    refactor(http_server): 3e94d41
    - embedd handler instead of implementing it
    - use NewServerMux as embedded handler to expose ServeHTTP

--- httpserver/server.go
+++ httpserver/server.go
@@ -13,14 +13,19 @@ type PlayerStore interface {
 
 type PlayerServer struct {
 	Store PlayerStore
-	router *http.ServeMux
+	http.Handler // PlayerServer now exposes all apis of this embedding
 }
 
 func NewPlayerServer(store PlayerStore) *PlayerServer {
-	p := &PlayerServer{store, http.NewServeMux()}
+	p := new(PlayerServer)
 
-	p.router.Handle("/players/", http.HandlerFunc(p.playersHandler))
-	p.router.Handle("/league", http.HandlerFunc(p.leagueHandler))
+	p.Store = store
+
+	router := http.NewServeMux()
+	router.Handle("/players/", http.HandlerFunc(p.playersHandler))
+	router.Handle("/league", http.HandlerFunc(p.leagueHandler))
+
+	p.Handler = router
 
 	return p
 }
@@ -37,10 +42,6 @@ func (p *PlayerServer) playersHandler (w http.ResponseWriter, r *http.Request) {
 
 func (p *PlayerServer) leagueHandler (w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-}
-
-func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	p.router.ServeHTTP(w, r)
 }
 
 func (p *PlayerServer) processGet(w http.ResponseWriter, player string) {
