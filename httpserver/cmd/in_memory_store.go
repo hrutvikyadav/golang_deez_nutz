9bbca6aa6db74515a594cbb0e620cc5b0cc40bcf
hrutvikyadav <hrutvikyadav@gmail.com>
2026-02-18 15:53:18 +0530

    refactor(http_server): main

---
+++ httpserver/cmd/in_memory_store.go
@@ -0,0 +1,22 @@
+package main
+
+type InMemoryStore struct {
+	// the store will be accessed by http handlers.
+	// in go for each request, new goroutine is spinned up
+	// data race for 👇
+	scores map[string]int
+	// TODO: put it under a mutex, use locking to avoid race conditions
+}
+
+func NewInMemoryStore() *InMemoryStore {
+	return &InMemoryStore{make(map[string]int)}
+}
+
+func (ims *InMemoryStore) GetScore(name string) int {
+	return ims.scores[name]
+}
+
+func (ims *InMemoryStore) PostWin(name string) {
+	ims.scores[name]++ // adding entry to nil map? solve by constructor
+}
+
--- httpserver/cmd/main.go
+++ httpserver/cmd/main.go
@@ -7,22 +7,6 @@ import (
 	go_httpserver "github.com/hrutvikyadav/go-httpserver"
 )
 
-type InMemoryStore struct {
-	scores map[string]int
-}
-
-func NewInMemoryStore() *InMemoryStore {
-	return &InMemoryStore{make(map[string]int)}
-}
-
-func (ims *InMemoryStore) GetScore(name string) int {
-	return ims.scores[name]
-}
-
-func (ims *InMemoryStore) PostWin(name string) {
-	ims.scores[name]++ // adding entry to nil map? solve by constructor
-}
-
 func main () {
 	server := &go_httpserver.PlayerServer{&InMemoryStore{}}
 	log.Fatal(http.ListenAndServe(":8099", server))
